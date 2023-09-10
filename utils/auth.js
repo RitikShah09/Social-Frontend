645c5a8ea1ac248d1f40a5a9efcbffb0781c7fe4
Your Name <you@example.com>
2023-09-10 13:56:35 +0530

    update

--- utils/auth.js
+++ utils/auth.js
@@ -2,17 +2,22 @@
 import { useCookies } from "react-cookie";
 import { redirect } from "next/navigation";
 export default function redirToSignInIfNoToken() {
-  const userData = localStorage.getItem("UserDetails");
+  if (typeof window !== "undefined") {
+    const userData = localStorage.getItem("UserDetails");
     const [cookie, setCookie] = useCookies(["token"]);
-  if (!cookie.token || !userData) {
-    return redirect("/login");
+    if (!cookie.token || !userData) {
+      return redirect("/login");
+    }
   }
-};
+}
+ 
 
 export const redirectToHome = () => {
+  if (typeof window !== "undefined") {
   const userData = localStorage.getItem("UserDetails");
   const [cookie, setCookie] = useCookies(["token"]);
   if (cookie.token || userData) {
     return redirect("/home");
+  }
   }
 }
